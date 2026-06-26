a60d596ff6da2dd18f32effed2f043c21f378502
galina-s-user <galina.sosnovskaya@gmail.com>
2026-06-26 18:43:06 +0300

    First Commit

---
+++ app.js
@@ -0,0 +1,7 @@
+//const firstName = 'Galya'
+//firstName = 'Ivan'
+//console.log('Hellooooooooo', firstName )
+
+let firstName = 'Ivan'
+firstName = 'Olya'
+console.log('Hellooooo,', firstName, '!')
