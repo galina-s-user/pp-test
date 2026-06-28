3ab972ef2121db679cf534843247563c973f6de7
galina-s-user <galina.sosnovskaya@gmail.com>
2026-06-28 12:20:01 +0300

    Новый app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-let firstName = 'Galya'
-let age = '34'
-let city = 'Ivanovo'
-let hobby = 'qigong'
+firstName = 'Galya';
+let age = '34';
+let city = 'Ivanovo';
+let hobby = 'qigong';
 console.log('Hello.', 'A few words about me:', firstName, age, 'years old.', 'I live in', city, 'and my hobby is', hobby, '.')
