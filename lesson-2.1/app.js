88e57dc6df035b850bf8f62e9be55cc2c1228315
galina-s-user <galina.sosnovskaya@gmail.com>
2026-06-30 13:18:35 +0300

    Lesson 2-2

--- lesson-2.1/app.js
+++
@@ -1,5 +0,0 @@
-firstName = 'Galya';
-let age = '34';
-let city = 'Ivanovo';
-let hobby = 'qigong';
-console.log('Hello.', 'A few words about me:', firstName, age, 'years old.', 'I live in', city, 'and my hobby is', hobby, '.')
